d63724c7d3fe368113a497fa8dd5559ef9776e51
Ivaylo Todorov <ivoo2006@abv.bg>
2021-02-24 12:34:05 +0000

    pm2 ecosystem.config.js updated with key

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,14 +11,15 @@ module.exports = {
   ],
   deploy: {
     production: {
-      user: 'nodejs',
-      host: '<CHANGE_TO_YOUR_HOST>',
+      key: 'C:/Users/test/ec2keypair.pem',
+      user: 'ec2-user',
+      host: 'ec2-18-134-249-95.eu-west-2.compute.amazonaws.com',
       ref: 'origin/master',
-      repo: '<CHANGE_TO_YOUR_GITHUB_REPO>',
+      repo: 'https://github.com/itodor01/meetup',
 
       // eslint-disable-next-line max-len
       // Make sure this directory exists on your server or change this entry to match your directory structure
-      path: '/home/nodejs/deploy',
+      path: '/home/ec2-user/deploy',
 
       'post-deploy': 'cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production',
     },
